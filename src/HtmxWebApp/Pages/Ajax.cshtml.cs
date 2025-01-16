27e8c2d1a6868d4773d5c4915af908651966561d
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-17 05:49:51 +0900

    Test hx-post

--- src/HtmxWebApp/Pages/Ajax.cshtml.cs
+++ src/HtmxWebApp/Pages/Ajax.cshtml.cs
@@ -2,11 +2,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HtmxWebApp.Pages;
 
+// hx-postで送信されたリクエストに対して、AntiforgeryTokenの検証を無効化
+[IgnoreAntiforgeryToken]
 public class AjaxModel : PageModel {
 	public void OnGet() {
 	}
 
-	public IActionResult OnGetPartial() {
-		return Partial("_Ajax");
-	}
+	private IActionResult PartialAjax() => Partial("_Ajax");
+
+	public IActionResult OnGetPartial() => PartialAjax();
+
+	public IActionResult OnPost() => PartialAjax();
 }
