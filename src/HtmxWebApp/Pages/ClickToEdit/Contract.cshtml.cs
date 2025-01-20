f5a5ec637f8c71ef970876165c0dfb37e54779ee
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-21 05:43:30 +0900

    Update pages

--- src/HtmxWebApp/Pages/ClickToEdit/Contract.cshtml.cs
+++ src/HtmxWebApp/Pages/ClickToEdit/Contract.cshtml.cs
@@ -1,6 +1,15 @@
 namespace HtmxWebApp.Pages.ClickToEdit;
 
 public class ContractModel : PageModel {
+	public bool IsEdit { get; set; } = false;
+
 	public void OnGet() {
 	}
+
+	// todo:
+	public void OnGetEdit() {
+		IsEdit = true;
+	}
+
+	// - OnPost
 }
