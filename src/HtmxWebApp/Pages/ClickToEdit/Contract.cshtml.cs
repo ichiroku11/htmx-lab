2010896767689cfff8146c5e8a038d3b7ceddb77
ichiroku11 <1648536+ichiroku11@users.noreply.github.com>
2025-01-22 05:38:09 +0900

    Update page

--- src/HtmxWebApp/Pages/ClickToEdit/Contract.cshtml.cs
+++ src/HtmxWebApp/Pages/ClickToEdit/Contract.cshtml.cs
@@ -1,14 +1,28 @@
 namespace HtmxWebApp.Pages.ClickToEdit;
 
+public class Contract {
+	public string Surname { get; set; } = "";
+	public string GivenName { get; set; } = "";
+	public string MailAddress { get; set; } = "";
+}
+
 public class ContractModel : PageModel {
 	public bool IsEdit { get; set; } = false;
 
+	public Contract Contract { get; set; } = new Contract();
+
 	public void OnGet() {
+		Contract.GivenName = "Taro";
+		Contract.Surname = "Yamada";
+		Contract.MailAddress = "taro@example.jp";
 	}
 
 	// todo:
 	public void OnGetEdit() {
 		IsEdit = true;
+		Contract.GivenName = "Taro";
+		Contract.Surname = "Yamada";
+		Contract.MailAddress = "taro@example.jp";
 	}
 
 	// - OnPost
